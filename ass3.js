47d1e10c2a03578ab78d7f396a7fd40f1092d3e6
Marcus Lindstedt <marcus.t.lindstedt@gmail.com>
2016-12-13 11:25:57 +0100

    Finalized assignment 3 with form submission.

--- ass3.js
+++ ass3.js
@@ -10,6 +10,7 @@
   var addItemButton = document.getElementById("add-item");
   var removeItemButton = document.getElementById("remove-item");
   var buttons = document.getElementsByClassName("remove-list-item");
+  var form = document.getElementById("apply-for-pet");
 
   for(var i = 0; i < buttons.length; i++){
     buttons[i].addEventListener("click", removeElementFromList, false);
@@ -20,6 +21,11 @@
   infoButton.addEventListener("click", function() { changeDiv("info")}, false);
   addItemButton.addEventListener("click", promptMessage, false);
   removeItemButton.addEventListener("click", removeItemInList, false);
+  form.addEventListener("submit", function(event){
+    event.preventDefault();
+    submitInfo(this.elements);
+  }, false);
+
 
 // Function for changing the Div tag
 function changeDiv(button){
@@ -49,15 +55,57 @@ function removeItemInList(){
     theList.removeChild(theList.lastElementChild);
   }
 }
+
 // Part 4 Remove element using this
 
 // Removes an element from a list, after user confirmation.
 	function removeElementFromList(){
 
-		if(window.confirm("By clicking yes you are allowing the almighty Lord to remove this  taint!")){
+		if(window.confirm("By clicking yes you are allowing the almighty Lord to remove this taint!")){
 			var parentNode = buttons.parentNode;
 			this.parentNode.remove(this);
 		}
 	}
 
     // Part 5 Forms
+
+function submitInfo(info){
+  var isChecked = false;
+  for(var i = 0; i < info.pet.length; i++){
+    if(info.pet[i].checked)
+    isChecked = true;
+  }
+
+  if(info.firstname.value.length <= 0 || info.firstname.value.length >= 50){
+    alert("Invalid input for first name");
+  }
+
+  else if(info.lastname.value.length <= 0 || info.lastname.value.length >= 50){
+    alert("Invalid input for last name");
+  }
+
+  else if(info.age.value.match(/^\d+$/)){
+    alert("Age must be a number");
+  }
+
+  else if(info.age.value < 0 || info.age.value == ""){
+    alert ("Negative number or no number added");
+  }
+
+  else if(info.email.value.length <= 0 || info.lastname.value.length >= 50){
+    alert("Invalid input for e-mail");
+  }
+
+  else if(!isChecked){
+    alert("You need to choose which pet you prefer");
+  }
+
+  else{
+    console.log("First name: " + info.firstname.value + "\n" +
+                "Last name: " + info.lastname.value + "\n" +
+                  "Age: " + info.age.value + "\n" +
+                "E-mail: " + info.email.value);
+
+  event.target.submit();
+  }
+}
